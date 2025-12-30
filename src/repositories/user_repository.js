668182fbdf99251ca805e298510c970d487467ac
Emanuele Contini <emanuelecontini26@gmail.com>
2025-12-30 15:44:00 +0100

    feat: add repository and service functions to update user data

--- src/repositories/user_repository.js
+++ src/repositories/user_repository.js
@@ -79,15 +79,65 @@ async function get_mentors(name, surname, email, availability_day, sector, last_
 
     values.push(limit);
     index++;
-    query += ` LIMIT $${index}`;
+    query += `ORDER BY id \nLIMIT $${index}`;
 
     const result = await pool.query(query, values);
     return result.rows;
 }
 
+async function update_name(user_id, new_name) {
+    const query = `
+        UPDATE users
+        SET name = $1
+        WHERE id = $2
+        RETURNING id, name;
+    `;
+  const result = await pool.query(query, [new_name, user_id]);
+  return result.rows[0];
+}
+
+async function update_surname(user_id, new_surname) {
+    const query = `
+        UPDATE users
+        SET surname = $1
+        WHERE id = $2
+        RETURNING id, surname;
+    `;
+  const result = await pool.query(query, [new_surname, user_id]);
+  return result.rows[0];
+}
+
+async function update_bio(user_id, new_bio) {
+    const query = `
+        UPDATE users
+        SET bio = $1
+        WHERE id = $2
+        RETURNING id, bio;
+    `;
+  const result = await pool.query(query, [new_bio, user_id]);
+  return result.rows[0];
+}
+
+async function update_photo_url(user_id, new_url) {
+    const query = `
+        UPDATE users
+        SET photo_url = $1
+        WHERE id = $2
+        RETURNING id, photo_url;
+    `;
+  const result = await pool.query(query, [new_url, user_id]);
+  return result.rows[0];
+}
+
+
+
 module.exports = {
     get_users,
     register_user,
     get_login_data,
-    get_mentors
+    get_mentors,
+    update_name,
+    update_surname,
+    update_bio,
+    update_photo_url
 };
