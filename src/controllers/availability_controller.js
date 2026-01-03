277045eb00782a3dddce2bd8545b7d2e463d539f
Emanuele Contini <emanuelecontini26@gmail.com>
2026-01-03 13:26:48 +0100

    feat: add remove availability endpoints and dtos

--- src/controllers/availability_controller.js
+++ src/controllers/availability_controller.js
@@ -4,15 +4,35 @@ const availability_service = require('../services/availability_service.js');
 const auth = require('../middleware/auth_middleware.js');
 const validate = require('../dtos/dto_middleware.js');
 const add_availability_dto = require('../dtos/mentor_availability/add_availability_dto.js');
+const remove_availability_dto = require('../dtos/remove_availability_dto.js');
 
 router.post('/add_availability', validate(add_availability_dto), auth, async (req, res) => {
     const mentor_id = req.user.id;
     const result = await availability_service.add_availability(mentor_id, req.body);
+    const {id, weekday, start_time, end_time} = result;
     return res.status(201).json({
         message: 'Availability added',
         success: true,
-        data: result
+        data: id, weekday, start_time, end_time
     });
+});
+
+router.get('/get_availabilities', auth, async (req, res) => {
+    const mentor_id = req.user.id;
+    const dates = await availability_service.get_availabilities(mentor_id, req.body);
+    res.status(200).json({
+        message: 'All dates fetched',
+        success: true,
+        data: dates
+    });
+});
+
+router.delete('/remove_availability', validate(remove_availability_dto), auth, async (req, res) => {
+
+});
+
+router.delete('/remove_all_availability', auth, async (req, res) => {
+    
 });
 
 module.exports = router;
--- src/services/availability_service.js
+++ src/services/availability_service.js
@@ -9,8 +9,37 @@ async function add_availability(mentor_id, payload) {
         throw err;
     }
     return result;
+};
+
+async function get_availabilities(mentor_id) {
+    const dates = await availability_repository.get_availabilities(mentor_id);
+    if(!dates) {
+        dates = 'No availability found';
+    }
+    return dates;
+};
+
+async function remove_availability(id, mentor_id) {
+    const date = availability_repository.remove_availability(id, mentor_id);
+    if(!date) {
+        const err = new Error('Availability not found');
+        err.status(404);
+        throw err;
+    }
+    return date;
+}
+
+async function remove_all(mentor_id) {
+    const dates = availability_repository.remove_all(mentor_id);
+    if(!dates) {
+        dates = 'No date to remove';
+    }
+    return dates; 
 }
 
 module.exports = {
-    add_availability
+    add_availability,
+    get_availabilities,
+    remove_availability,
+    remove_all
 };
