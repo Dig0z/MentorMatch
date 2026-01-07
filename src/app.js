b7cf6362e26f3b4e6bc6e3ffd68da1b56953e215
Emanuele Contini <emanuelecontini26@gmail.com>
2026-01-07 12:41:21 +0100

    feat: create google_auth_repository to manage and store google token for Meet in database

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ require('dotenv').config({path: path.resolve(__dirname, '..', '.env')});
 const exception_handler = require('./middlewares/exception_handler.js');
 const express = require('express');
 const app = express();
-const google = require('./google.js');
+const google_auth_service = require('./services/google_auth_service.js');
 
 require('./config/db.js');
 
@@ -13,9 +13,9 @@ const routes = require('./routes.js');
 app.use('/api', routes);
 app.use(exception_handler);
 
-google.loadSavedTokens();
+await google_auth_service.loadSavedTokens();
 
-const PORT = (process.env.PORT || 3000);
+const PORT = (process.env.PORT || 5000);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
--- src/services/google_auth_service.js
+++ src/services/google_auth_service.js
@@ -1,5 +1,8 @@
 const {google} = require('googleapis');
-const fs = require('fs');
+const google_auth_repository = require('../repositories/google_auth_repository.js');
+const { ideahub } = require('googleapis/build/src/apis/ideahub/index.js');
+
+const saltRounds = 10;
 
 const client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
@@ -16,16 +19,46 @@ function getAuthUrl() {
 }
 
 async function setAuthCode(code) {
-    const { tokens } = await client.getToken(code);
-    fs.writeFileSync('google_tokens.json', JSON.stringify(tokens));
-    client.setCredentials(tokens);
+    const {tokens} = await client.getToken(code);
+    const {access_token, refresh_token, scope, token_type, expiry_date} = tokens;
+    const old_token = await google_auth_repository.get_tokens();
+    if(!refresh_token && !old_token) {
+        const err = new Error('Missing refresh token');
+        err.status = 404;
+        throw err;
+    }
+    let cypher_refresh_token;
+    if(refresh_token) {
+        cypher_refresh_token = cypher(refresh_token);
+    } else if(old_token) {
+        const {refresh_token:old_refresh} = old_token;
+        cypher_refresh_token = old_refresh;
+    }
+    const cypher_access_token = cypher(access_token);    
+    let result;
+    if(!old_token) {
+        result = await google_auth_repository.insert_token(cypher_access_token, cypher_refresh_token, scope, token_type, expiry_date);
+    } else {
+        const {id} = old_token;
+        result = await google_auth_repository.update_token(id, cypher_access_token, cypher_refresh_token, scope, token_type, expiry_date);
+    }
+    const safe_refresh_token = decypher(cypher_refresh_token);
+    const correct_tokens = {access_token, refresh_token:safe_refresh_token, scope, token_type, expiry_date};
+    client.setCredentials(correct_tokens);
 }
 
-function loadSavedTokens() {
-    if (fs.existsSync('google_tokens.json')) {
-        const tokens = JSON.parse(fs.readFileSync('google_tokens.json'));
-        client.setCredentials(tokens);
+async function loadSavedTokens() {
+    const tokens = await google_auth_repository.get_tokens();
+    if(!tokens) {
+        const err = new Error('Tokens not found');
+        err.status = 404;
+        throw err;
     }
+    const {access_token:cypher_access_token, refresh_token:cypher_refresh_token, scope, token_type, expiry_date} = tokens;
+    const access_token = decypher(cypher_access_token);
+    const refresh_token = decypher(cypher_refresh_token);
+    const plain_tokens = {access_token, refresh_token, scope, token_type, expiry_date};
+    client.setCredentials(plain_tokens);
 }
 
 async function createMeetLink() {
@@ -45,6 +78,34 @@ async function createMeetLink() {
     });
 
   return res.data.hangoutLink;
+}
+
+function cypher(string) {
+    let cypher_string = '';
+    for(let i = 0; i < string.length; i++) {
+        const char_code = string.charCodeAt(i);
+        let new_char_code = ((char_code + 3) % 126);
+        if(new_char_code < 32) {
+            new_char_code += 32;
+        }
+        const new_char = String.fromCharCode(new_char_code);
+        cypher_string += new_char;
+    }
+    return cypher_string;
+}
+
+function decypher(cypher_string) {
+    let string = '';
+    for(let i = 0; i < cypher_string.length; i++) {
+        const cypher_char_code = cypher_string.charCodeAt(i);
+        let new_char_code = (cypher_char_code - 3);
+        if(new_char_code < 32) {
+        new_char_code += 32;
+        }
+        const new_char = String.fromCharCode(new_char_code);
+        string += new_char;
+    }
+    return string;
 }
 
 module.exports = {
